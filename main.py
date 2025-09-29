1ae90b72e90fe2649272b2072a09465f479212de
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-09-29 19:19:40 +0600

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from modules.my import my_command
 from modules.admin import admin_command
 from modules.me import me_command
 from modules.info import info_command
+from modules.link import link_command
 from modules.fwd import handle_forwarded_message
 from modules.username import username_command
 from modules.inline import inline_query_handler, chosen_inline_result
@@ -16,4 +17,3 @@ from shared.chatinfo import handle_message
 
 LOGGER.info("QuickInfoBot Started Successfully 💥")
 bot.run()
-
